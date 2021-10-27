aa56e54f059c498fce642cab3d91436d31c49978
Anupam751 <81550210+Anupam751@users.noreply.github.com>
2021-10-27 16:26:58 +0530

    Add files via upload

---
+++ labsheet1/Exercise3.java
@@ -0,0 +1,19 @@
+package labsheet1;
+
+public class Exercise3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0,b=1,c,n=10;
+		System.out.println(a + " " + b);
+		
+		for(int i = 2;i<n;i++)
+		{
+			c = a + b;
+			System.out.println(" " + c);
+			a = b;
+			b = c;
+		}
+	}
+
+}
